c0a483ba9b240fa0fce816994db15a0a6f0cf9d2
AlexB <alex.borgmeyer@web.de>
2023-07-08 12:45:08 +0200

    prettier output

--- main_sum_of_squares.cpp
+++ main_sum_of_squares.cpp
@@ -8,14 +8,22 @@ int main(){
     }
     */
 
-    int upper_limit_num = 1000000;
-    int upper_limit_sum = 4;
-    for (int i = 1; i < upper_limit_num; i++){
-        for(int j = 3; j < upper_limit_sum; j++){
+    //limits for numbers checked
+    int lower_limit_num = 1;
+    int upper_limit_num = 10000000;
+
+    //limits for number of summands
+    int lower_limit_sum = 3;
+    int upper_limit_sum = 3;
+
+    int false_or_true = 0; //set 1 to print true results, set 0 to print false results
+
+    for (int i = lower_limit_num; i < upper_limit_num + 1; i++){
+        for(int j = lower_limit_sum; j < upper_limit_sum + 1; j++){
             std::vector<int> calc = sum_of_n(i,j);
 
             //only print hits
-            if(calc.at(0) == 0){                 
+            if(calc.at(0) == false_or_true){                 
                 std::cout << i << "-->" << calc.at(0) << "||";
                 for(int k = 1; k < j+1; k++){
                     std::cout << calc.at(k) << ",";
